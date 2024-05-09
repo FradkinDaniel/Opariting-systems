59fa0a1845fba84ba75edfa363ce8d65fec8ef11
Daniel Fradkin <dtselon@gmail.com>
2024-05-09 15:03:46 +0300

    ex3

---
+++ src/Compond.java
@@ -0,0 +1,16 @@
+public class Compond {
+    public void eat(int i){
+        System.out.println(i+ "done eating");
+    }
+    public  void drink(int i){
+        static System.out.println(i+"done drinking");
+    }
+    static public void milk(int i){
+        System.out.println(i+"done giving milk");
+    }
+    static public void walk(int i){
+        System.out.println(i+"done walking");
+    }
+}
+
+}
---
+++ src/SharedReasources.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.*;
+public class SharedReasources {
+    Semaphore hay = new Semaphore(6);
+    Semaphore drinking = new Semaphore(3);
+    Semaphore milking = new Semaphore(5);
+    Semaphore walking = new Semaphore(0);
+
+    synchronized public void  allowWalk(){
+        walking.release();
+        walking.release();
+        walking.release();
+        walking.release();
+    }
+    public void drinking(int i){
+
+    }
+}
